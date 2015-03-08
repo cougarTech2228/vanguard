4ce46769cfb51fa2402eddb91d5c05924e25427d
Wyatt J. Brown <sushidudeteam@gmail.com>
2015-03-08 08:58:05 -0400

    Format all HTML, JS, and CSS files that are not minimized. Tabs are the prefered method of indentation but js-beautify version 1.5.5 has a bug where it will not indent HTML or CSS with tabs. While it is not optimal, all indention will be with spaces in order to maintain consistency. find -type f -regextype posix-extended ! -regex '.*\.min\..*' -regex '.*\.(html|js|css)' -exec sh -c 'js-beautify -Inr -w 0 "{}" && temporary_file="$(mktemp)" && git stripspace < "{}" > "$temporary_file" && mv -f "$temporary_file" "{}"' \;

--- vanguard/src/lib/link.js
+++ vanguard/src/lib/link.js
@@ -1,35 +1,35 @@
-Link = function(){
-     this.listening = false;   
-        
-     function link(url){
-         $("html").fadeOut(function () {
-             // when the animation is complete, set the new location
-             location = url;
-         });
-     }
-     
-     document.addEventListener("keydown", function ( event ) {
-          event = event || window.event;
-          key = event.keyCode;          
-          
-          if(key == 191){
-               this.listening= !this.listening;
-               event.preventDefault();
-          }else if(this.listening){
-               if(key==65){
-                    link("/picture.html");
-               }else if(key==83){
-                    link("/input.html");
-               }else if(key==68){
-                    link("/vanguard.html");
-               }else if(key==70){
-                    link("/checklist.html");
-               }
-               
-               this.listening= !this.listening;
-               event.preventDefault();
-          }
-     });
+Link = function() {
+    this.listening = false;
+
+    function link(url) {
+        $("html").fadeOut(function() {
+            // when the animation is complete, set the new location
+            location = url;
+        });
+    }
+
+    document.addEventListener("keydown", function(event) {
+        event = event || window.event;
+        key = event.keyCode;
+
+        if (key == 191) {
+            this.listening = !this.listening;
+            event.preventDefault();
+        } else if (this.listening) {
+            if (key == 65) {
+                link("/picture.html");
+            } else if (key == 83) {
+                link("/input.html");
+            } else if (key == 68) {
+                link("/vanguard.html");
+            } else if (key == 70) {
+                link("/checklist.html");
+            }
+
+            this.listening = !this.listening;
+            event.preventDefault();
+        }
+    });
 
 }
 
