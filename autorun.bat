aca922e3565e2326b80c0705eadb047e8b2932a3
Erich Spaker <erichspaker@gmail.com>
2015-03-07 23:02:41 -0500

    autorun

--- autorun.bat
+++ autorun.bat
@@ -1,2 +1,7 @@
-.\dart\dart-sdk\bin\dart.exe .\vanguard\bin\server
-rt "" http://127.0.0.1:8080/vanguard.html
+taskkill /IM notepad.exe
+
+cd vanguard/bin/
+
+..\..\dart\dart-sdk\bin\dart.exe server.dart
+
+start "" http://127.0.0.1:8080/vanguard.html
