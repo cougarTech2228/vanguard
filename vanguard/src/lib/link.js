73b218a9d95be242f650622a4f48be1e78e20a1b
blueForestIcarus <erichspaker@gmail.com>
2015-03-10 00:19:25 -0400

    made some improvements, inclueding help text on input

--- vanguard/src/lib/link.js
+++ vanguard/src/lib/link.js
@@ -1,5 +1,8 @@
 Link = function() {
     this.listening = false;
+    this.info_bar = document.createElement('div');
+    this.info_bar.id="info_bar"
+    document.body.appendChild(this.info_bar);
 
     function link(url) {
         $("html").fadeOut(function() {
@@ -7,32 +10,7 @@ Link = function() {
             location = url;
         });
     }
-
-    document.addEventListener("keydown", function(event) {
-        event = event || window.event;
-        key = event.keyCode;
-
-        if (key == 191) {
-            this.listening = !this.listening;
-            event.preventDefault();
-        } else if (this.listening) {
-            this.listening = !this.listening;
-            event.preventDefault();
-
-            if (key == 65) {
-                link("/picture.html");
-            } else if (key == 83) {
-                link("/input.html");
-            } else if (key == 68) {
-                link("/vanguard.html");
-            } else if (key == 70) {
-                link("/checklist.html");
-            } else {
-                command(key);
-            }
-
-        }
-    });
+    
     container = document.createElement('div');
     container.className = "github_container";
 
@@ -42,7 +20,64 @@ Link = function() {
 
     container.appendChild(github);
     document.querySelector("body").appendChild(container);
+    
+        document.addEventListener("keydown", function(event) {
+        event = event || window.event;
+        key = event.keyCode;
+
+        if (key == 191) {
+            this.listening = !this.listening;
+            linker.info_open();
+            linker.info_text("?");
+            event.preventDefault();
+            
+        } else if (this.listening) {
+            this.listening = !this.listening;
+            event.preventDefault();
+
+            if (key == 65) {
+                 linker.info_text("switching to laserscope");
+                link("/picture.html");
+            } else if (key == 83) {
+                 linker.info_text("switching to spearhead");
+                link("/input.html");
+            } else if (key == 68) {
+                 linker.info_text("switching to datacrystal");
+                link("/vanguard.html");
+            } else if (key == 70) {
+                 linker.info_text("switching to aquapipe");
+                link("/checklist.html");
+            } else {
+                try{
+                  command(key);
+                }catch(e){
+                  linker.info_say("unknown command")
+                }
+            }
+
+        }
+    });
 }
+
+Link.prototype.info_say = function (text){
+  delay = 2000;
+  this.info_open();
+  this.info_text(text);
+  setTimeout(this.info_close,delay);
+}
+
+Link.prototype.info_open = function (text){
+  this.info_bar.style.opacity=.8;
+}
+
+Link.prototype.info_text = function (text){
+  this.info_bar.innerHTML = text;
+}
+
+Link.prototype.info_close = function (text){
+  this.info_bar.style.opacity=0;
+}
+    
 
 function queryParameter(name){
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -51,4 +86,4 @@ function queryParameter(name){
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-new Link();
+linker = new Link();
