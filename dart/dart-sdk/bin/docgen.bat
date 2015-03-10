7ab6d11ae76700b2f56552e6d037f1f50e93dd98
blueForestIcarus <erichspaker@gmail.com>
2015-03-09 20:09:22 -0400

    the files are now in /dart because you broke the git ignore

---
+++ dart/dart-sdk/bin/docgen.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+REM for details. All rights reserved. Use of this source code is governed by a
+REM BSD-style license that can be found in the LICENSE file.
+
+echo "The 'docgen.bat' name is deprecated. Prefer 'dartdocgen.bat' instead."
+dartdocgen %*
